e21e9eb85906ae6ee94eee55802a072053784b05
Sam Pohlenz <sam@sampohlenz.com>
2019-12-20 15:47:54 +1030

    Preliminary implementation of admin/resource authorization

--- lib/trestle/auth.rb
+++ lib/trestle/auth.rb
@@ -6,19 +6,28 @@ module Trestle
   module Auth
     extend ActiveSupport::Autoload
 
+    autoload :Admin
+    autoload :AccessDenied
     autoload :Backends
+    autoload :Builder
     autoload :Configuration
     autoload :Constraint
     autoload :ControllerMethods
     autoload :ModelMethods
     autoload :NullUser
+    autoload :Resource
 
     module Controller
       extend ActiveSupport::Autoload
 
       autoload :Authentication
+      autoload :Authorization
       autoload :Locale
       autoload :TimeZone
+    end
+
+    autoload_under "adapters" do
+      autoload :CanCanAdapter
     end
   end
 
